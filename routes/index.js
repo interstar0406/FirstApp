b6ec9b2de60fc6972e9e45502766e6ef029aacac
Tan Nguyen <minhtan1707@gmail.com>
2018-08-30 20:38:08 +0700

    no message

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  var obj;
+	fs.readFile('./db/home.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+    res.render('index', { data1: obj });
+  });
+  
+});
+
+module.exports = router;
---
+++ routes/shop.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+	var obj;
+		fs.readFile('./db/shop.json', 'utf8', function (err, data) {
+		if (err) throw err;
+		obj = JSON.parse(data);
+		res.render('shop', { shopitem: obj });
+	});
+	
+  });
+  
+  module.exports = router;
