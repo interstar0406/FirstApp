52aed790792b686744ead8356b4487b1844b55f2
CườngTrần <interstar0406@gmail.com>
2018-08-30 18:58:22 +0700

    fix routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FullStack Vietnam' });
+router.get('/', function (req, res, next) {
+	var obj;
+
+	fs.readFile('./db/home.json','utf-8',function(err, data){
+		if(err) throw err;
+		obj.JSON.parse(data);
+		console.log(data);
+		res.render('users', {
+			title: 'Users List',
+			data: obj.lists
+		});
+	});
 });
-
 module.exports = router;
